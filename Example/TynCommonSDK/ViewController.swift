f81a15e5840d1c8035f51b5736b8127e09bc0f48
despacito2 <1756110665@qq.com>
2022-06-26 20:46:25 +0800

    Initial commit

---
+++ Example/TynCommonSDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TynCommonSDK
+//
+//  Created by 1756110665@qq.com on 06/26/2022.
+//  Copyright (c) 2022 1756110665@qq.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
